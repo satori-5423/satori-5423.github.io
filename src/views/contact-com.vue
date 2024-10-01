74b78eddd74758cfbe29f8cc20bf6b630b221582
satori-5423 <suxue2746841150@gmail.com>
2024-10-01 21:55:37 +0800

    update

--- src/views/contact-com.vue
+++ src/views/contact-com.vue
@@ -4,7 +4,7 @@
         <li>Discord: <a href="https://discordapp.com/users/1031115461238202391" target="_blank">__tyou</a></li>
         <li>Line: <a href="https://line.me/ti/p/0C9WUWk1NC" target="_blank">2746841150</a></li>
         <li>Telegream: <a href="https://t.me/satori_5423" target="_blank">@satori_5423</a></li>
-        <li>Twitter(X): <a href="https://x.com/__tyou_5423" target="_blank">@__tyou_5423</a></li>
+        <li>Twitter(X): <a href="https://x.com/tyou_5423" target="_blank">@tyou_5423</a></li>
         <li>Github: <a href="https://github.com/satori-5423" target="_blank">satori-5423</a></li>
     </ul>
 </template>
